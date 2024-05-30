14d7269e618a3cc9995f70b61b01bf932d087867
Luiz <luize1045@gmail.com>
2024-05-30 01:09:04 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  function job (){
+    Swal.fire("Estou trabalhando para atualizar o mais rápido possível!");
+}
